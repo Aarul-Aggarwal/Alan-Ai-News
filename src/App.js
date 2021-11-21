7ee2c8025b02dcb7c90e3bc534c036ab59f68460
Aarul Aggarwal <aggarwalaarul@gmail.com>
2021-11-21 21:44:59 +0530

    alan ai news project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web"
+import NewsCards from "./components/NewsCards/NewsCards";
+import useStyles from './styles'
+import wordsToNumbers from "words-to-numbers"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const alanKey = 'b699545ad483964d93274fcfce5957472e956eca572e1d8b807a3e2338fdd0dc/stage'
+
+const App = () => {
+
+    const [newsArticles, setNewsArticles] = useState([]);
+    const classes = useStyles()
+    const [activeArticle, setActiveArticle] = useState(-1);
+
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({ command, articles, number }) => {
+                if (command === 'newHeadlines') {
+                    setNewsArticles(articles)
+                    setActiveArticle(-1)
+                } else if (command === 'highlight') {
+                    setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+                } else if (command === 'open') {
+                    const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+                    const article = articles[parsedNumber - 1];
+          
+                    if (parsedNumber > articles.length) {
+                      alanBtn().playText('Please try that again...');
+                    } else if (article) {
+                      window.open(article.url, '_blank');
+                      alanBtn().playText('Opening...');
+                    } else {
+                      alanBtn().playText('Please try that again...');
+                    }
+                }
+            }
+        })
+    }, [])
+
+    return(
+        <div>
+            <div className={classes.logoContainer}>
+                <img src="https://artificialintelligence-news.com/wp-content/uploads/sites/9/2020/03/ai-newsv4-2-svg.png" className={classes.alanLogo} alt="Oops! BTW this is the LOGO nothing important" />
+            </div>
+            <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+        </div>
+    )
 }
 
-export default App;
+export default App
